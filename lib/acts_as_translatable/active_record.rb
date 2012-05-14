cebcf52baecb18d97dca563ff427fffdc7dc1afa
Lasse Bunk <lassebunk@gmail.com>
2012-05-14 10:25:39 +0200

    add active record methods

--- lib/acts_as_translatable/active_record.rb
+++ lib/acts_as_translatable/active_record.rb
@@ -1,50 +1,52 @@
+require 'acts_as_translatable/instance_methods'
+
 class ActiveRecord::Base
   def self.acts_as_translatable_on(*fields)
-    # after save hook
-    self.after_save :save_translations
+    include ActsAsTranslatable::InstanceMethods
     
-    # get the fields
-    @translatable_fields = fields
-    
-    # loop through fields
     fields.each do |field|
-      eval "def #{field}
-              translations[I18n.locale] ||= {}
-              translations[I18n.locale][\"#{field}\"]
-            end
+      eval "class ::#{name}
+              after_save :save_translations
+              
+              def #{field}
+                translations[I18n.locale] ||= {}
+                translations[I18n.locale][\"#{field}\"]
+              end
+
+              def #{field}=(value)
+                translations[I18n.locale] ||= {}
+                translations[I18n.locale][\"#{field}\"] = value
+              end
+
+              def translations
+                unless @translations
+                  @translations = {}
+                  record_translations.each do |translation|
+                    @translations[translation.locale.to_sym] ||= {}
+                    @translations[translation.locale.to_sym][translation.translatable_field] = translation.content
+                  end
+                end
+                @translations
+              end
+
+              def record_translations
+                @record_translations ||= RecordTranslation.where(:translatable_id => id, :translatable_type => self.class.name)
+              end
             
-            def #{field}=(value)
-              translations[I18n.locale] ||= {}
-              translations[I18n.locale][\"#{field}\"] = value
-            end
-            
-            def translations
-              unless @translations
-                @translations = {}
-                record_translations.each do |translation|
-                  @translations[translation.locale.to_sym] ||= {}
-                  @translations[translation.locale.to_sym][translation.translatable_field] = translation.content
+              def save_translations
+                # delete all previous record translations
+                record_translations.destroy_all
+
+                # loop through updated translations
+                translations.each_pair do |locale, fields|
+                  fields.each_pair do |field, content|
+                    # create translation record
+                    RecordTranslation.create :translatable_id => id, :translatable_type => self.class.name, :translatable_field => field, :locale => locale.to_s, :content => content unless content.blank?
+                  end
                 end
               end
-              @translations
-            end
-            
-            def record_translations
-              @record_translations ||= RecordTranslation.where(:translatable_id => id, :translatable_type => self.class.name)
-            end"
-    end
-  end
-  
-  def save_translations
-    # delete all previous record translations
-    record_translations.destroy_all
-    
-    # loop through updated translations
-    translations.each_pair do |locale, fields|
-      fields.each_pair do |field, content|
-        # create translation record
-        RecordTranslation.create :translatable_id => id, :translatable_type => self.class.name, :translatable_field => field, :locale => locale.to_s, :content => content unless content.blank?
-      end
+            end"  
     end
   end
 end
+
